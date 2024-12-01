345f51061075b8ebcdca05cef8a79c6394de7276
Kevin Lanni <kmlanni91@gmail.com>
2024-12-01 12:49:54 -0500

    feat: add cli scaffolding

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use std::fmt::Debug;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+
+}
+
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use clap::Parser;
+use aoc;
+
+
+fn main() {
+    let cli = aoc::Cli::parse();
+}
+
