0ca9c08745e5d1611cbcb5b26f744f76f5a22623
Kevin Lanni <kmlanni91@gmail.com>
2024-12-08 17:39:34 -0500

    feat: non working day 2 solution

--- src/day2.rs
+++ src/day2.rs
@@ -13,7 +13,7 @@ impl Run for Runner {
         Ok(u64::try_from(
             reports
                 .iter()
-                .map(|report| analyze_report(report))
+                .map(|report| analyze_report(report, false))
                 .filter(|safety| *safety == Safety::Safe)
                 .count(),
         )
@@ -21,8 +21,18 @@ impl Run for Runner {
     }
 
     #[allow(refining_impl_trait)]
-    fn run2(&self, _reader: impl BufRead) -> Result<u64, InputParseError> {
-        Ok(0)
+    fn run2(&self, reader: impl BufRead) -> Result<u64, InputParseError> {
+        let reports = parse(reader)?;
+        //println!("{:?}", reports);
+        //println!("{}", reports.len());
+        Ok(u64::try_from(
+            reports
+                .iter()
+                .map(|report| analyze_report(report, true))
+                .filter(|safety| *safety == Safety::Safe)
+                .count(),
+        )
+        .expect("Count was bigger than u64"))
     }
 }
 
@@ -48,27 +58,35 @@ fn parse_line(line: String) -> Result<Vec<u32>, InputParseError> {
         .collect()
 }
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Clone, Copy)]
 enum Safety {
     Safe,
     UnSafe,
 }
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Clone, Copy)]
 enum Direction {
     Ascending,
     Descending,
     Neutral,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct ReportState {
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Skipper<'a> {
+    start: &'a Vec<u32>,
+    index: Option<u32>,
+    used: bool,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct ReportState<'a> {
     safety: Safety,
     direction: Option<Direction>,
     prev: Option<u32>,
+    skipper: Skipper<'a>,
 }
 
-fn analyze_report(report: &Vec<u32>) -> Safety {
+fn analyze_report(report: &Vec<u32>, dampen: bool) -> Safety {
     let safety = report
         .iter()
         .fold(
@@ -76,15 +94,20 @@ fn analyze_report(report: &Vec<u32>) -> Safety {
                 safety: Safety::Safe,
                 direction: None,
                 prev: None,
+                skipper: Skipper {
+                    start: report[0..4].iter().cloned().collect::<Vec<u32>>().as_ref(),
+                    index: None,
+                    used: false,
+                },
             },
-            |state, next| compute_state(state, *next),
+            |state, next| compute_state(state, *next, dampen),
         )
         .safety;
     //println!("{:?}", safety);
     safety
 }
 
-fn compute_state(state: ReportState, next: u32) -> ReportState {
+fn compute_state(state: ReportState, next: u32, dampen: bool) -> ReportState {
     let direction = match state.prev {
         Some(prev) => {
             let _dir = if prev < next {
@@ -115,17 +138,87 @@ fn compute_state(state: ReportState, next: u32) -> ReportState {
                 } else {
                     Safety::UnSafe
                 }
-            },
+            }
             (_, Some(Direction::Neutral)) => Safety::UnSafe,
-            (None, _) => if is_in_range {Safety::Safe} else {Safety::UnSafe},
+            (None, _) => {
+                if is_in_range {
+                    Safety::Safe
+                } else {
+                    Safety::UnSafe
+                }
+            }
             _ => Safety::Safe,
         },
-        Safety::UnSafe => Safety::UnSafe
+        Safety::UnSafe => Safety::UnSafe,
     };
-    ReportState {
-        safety,
-        direction,
-        prev: Some(next),
+    let index = state.skipper.index.map_or_else(|| 0, |idx| idx + 1);
+    match (&safety, &state.skipper.used, dampen) {
+        (Safety::UnSafe, false, true) => {
+            if index == 2 {
+                let dir_0_to_1 = get_direction(
+                    *state.skipper.start.get(0).unwrap(),
+                    *state.skipper.start.get(1).unwrap(),
+                );
+                let dir_0_to_3 = get_direction(
+                    *state.skipper.start.get(0).unwrap(),
+                    *state.skipper.start.get(3).unwrap(),
+                );
+                if dir_0_to_1 == dir_0_to_3 {
+                    ReportState {
+                        safety: Safety::Safe,
+                        direction: state.direction,
+                        prev: state.prev,
+                        skipper: Skipper {
+                            start: state.skipper.start,
+                            index: state.skipper.index,
+                            used: true
+                        }
+                    }
+                } else {
+                    ReportState {
+                        safety: Safety::Safe,
+                        direction,
+                        prev: Some(next),
+                        skipper: Skipper {
+                            start: state.skipper.start,
+                            index: Some(index),
+                            used: true
+                        }
+                    }
+                }
+            } else {
+                ReportState {
+                    safety: Safety::Safe,
+                    direction,
+                    prev: Some(next),
+                    skipper: Skipper {
+                        start: state.skipper.start,
+                        index: Some(index),
+                        used: true
+                    }
+                }
+            }
+        }
+        _ => ReportState {
+            safety,
+            direction,
+            prev: Some(next),
+            skipper: Skipper {
+                start: state.skipper.start,
+                index: Some(index),
+                used: state.skipper.used,
+            },
+        },
+    }
+}
+
+fn get_direction(first: u32, second: u32) -> Direction {
+    if first < second {
+        Direction::Ascending
+    } else if first < second {
+        Direction::Descending
+    } else {
+        Direction::Neutral
     }
 }
 
@@ -137,20 +230,32 @@ mod test {
     #[test]
     fn test_compute_state() {
         // Test initial state
+        let start = vec![7, 6, 4, 2];
         let result = compute_state(
             ReportState {
                 safety: Safety::Safe,
                 direction: None,
                 prev: None,
+                skipper: Skipper {
+                    start: &start,
+                    index: None,
+                    used: false,
+                },
             },
             7,
+            false,
         );
         assert_eq!(
             result,
             ReportState {
                 safety: Safety::Safe,
                 direction: None,
-                prev: Some(7)
+                prev: Some(7),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(0),
+                    used: false,
+                }
             }
         );
         // Test second state safe desc
@@ -159,15 +264,26 @@ mod test {
                 safety: Safety::Safe,
                 direction: None,
                 prev: Some(7),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(0),
+                    used: false,
+                },
             },
             6,
+            false,
         );
         assert_eq!(
             result2,
             ReportState {
                 safety: Safety::Safe,
                 direction: Some(Direction::Descending),
-                prev: Some(6)
+                prev: Some(6),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(1),
+                    used: false,
+                },
             }
         );
         // Test mid state safe desc
@@ -176,15 +292,26 @@ mod test {
                 safety: Safety::Safe,
                 direction: Some(Direction::Descending),
                 prev: Some(7),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(0),
+                    used: false,
+                },
             },
             6,
+            false,
         );
         assert_eq!(
             result3,
             ReportState {
                 safety: Safety::Safe,
                 direction: Some(Direction::Descending),
-                prev: Some(6)
+                prev: Some(6),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(1),
+                    used: false,
+                }
             }
         );
         // Test unsafe state neut
@@ -193,15 +320,26 @@ mod test {
                 safety: Safety::Safe,
                 direction: Some(Direction::Descending),
                 prev: Some(7),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(0),
+                    used: false,
+                },
             },
             7,
+            false,
         );
         assert_eq!(
             result4,
             ReportState {
                 safety: Safety::UnSafe,
                 direction: Some(Direction::Neutral),
-                prev: Some(7)
+                prev: Some(7),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(1),
+                    used: false,
+                }
             }
         );
         // Test unsafe dir change
@@ -210,15 +348,26 @@ mod test {
                 safety: Safety::Safe,
                 direction: Some(Direction::Descending),
                 prev: Some(7),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(0),
+                    used: false,
+                },
             },
             8,
+            false,
         );
         assert_eq!(
             result5,
             ReportState {
                 safety: Safety::UnSafe,
                 direction: Some(Direction::Ascending),
-                prev: Some(8)
+                prev: Some(8),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(1),
+                    used: false,
+                }
             }
         );
         // Test second state unsafe neut
@@ -227,15 +376,26 @@ mod test {
                 safety: Safety::Safe,
                 direction: None,
                 prev: Some(7),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(1),
+                    used: false,
+                },
             },
             7,
+            false,
         );
         assert_eq!(
             result6,
             ReportState {
                 safety: Safety::UnSafe,
                 direction: Some(Direction::Neutral),
-                prev: Some(7)
+                prev: Some(7),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(2),
+                    used: false,
+                }
             }
         );
         // Test second state unsafe out of range
@@ -244,24 +404,34 @@ mod test {
                 safety: Safety::Safe,
                 direction: None,
                 prev: Some(7),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(0),
+                    used: false,
+                },
             },
             11,
+            false,
         );
         assert_eq!(
             result6,
             ReportState {
                 safety: Safety::UnSafe,
                 direction: Some(Direction::Ascending),
-                prev: Some(11)
+                prev: Some(11),
+                skipper: Skipper {
+                    start: &start,
+                    index: Some(1),
+                    used: false,
+                }
             }
         );
-
     }
 
     #[test]
     fn test_analyze_report() {
         let input = vec![40, 42, 44, 47, 49, 50, 48];
-        let result = analyze_report(&input);
+        let result = analyze_report(&input, false);
         assert_eq!(result, Safety::UnSafe)
     }
 
@@ -286,10 +456,31 @@ mod test {
     }
 
     #[test]
-    fn part2() {
-        let input = String::from("");
+    fn test_analyze_report_with_dampen() {
+        // Test change in direction skip
+        let input = vec![40, 42, 39, 44, 47, 49, 50];
+        let result = analyze_report(&input, true);
+        assert_eq!(result, Safety::Safe);
 
-        let expected = 1;
+        let input2 = vec![40, 39, 42, 44, 47, 49, 50];
+        let result2 = analyze_report(&input2, true);
+        assert_eq!(result2, Safety::Safe);
+    }
+
+    #[test]
+    fn part2() {
+        let input = String::from(
+            "\
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9\
+        ",
+        );
+
+        let expected = 4;
         let result = Runner
             .run2(BufReader::new(&mut input.as_bytes()))
             .expect("Unexpected parse error");
